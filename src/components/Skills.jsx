76cbe2f4f9abb8ca89d51de0623a437fb53da62f
Vickeysvibe <vickeydvk4@gmail.com>
2024-04-29 15:00:44 +0530

    deleted:    public/logo192.png 	deleted:    public/logo512.png 	modified:   public/manifest.json 	modified:   src/App.css 	modified:   src/components/Skills.jsx 	modified:   src/index.css 	modified:   src/styles/home.css 	modified:   src/styles/sidepanel.css 	modified:   src/styles/skills.css 	modified:   src/styles/thoughts.css 	new file:   src/svgs/js.svg 	new file:   src/svgs/mongodb.svg 	modified:   src/svgs/next.svg 	new file:   src/svgs/node.svg 	new file:   src/svgs/python.svg 	modified:   src/svgs/react.svg

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,57 +1,36 @@
 import React from "react";
 import "../styles/skills.css";
 import react from "../svgs/react.svg";
-import node from "../images/node.png";
+import js from "../svgs/js.svg";
+import node from "../svgs/node.svg";
+import mongodb from "../svgs/mongodb.svg";
+import next from "../svgs/next.svg";
+import python from "../svgs/python.svg";
 export const Skills = () => {
   return (
     <div className="skills">
       <h1 className="topic">Skills</h1>
-      <div class="wrapper">
-        <div class="gallery">
-          <ul>
-            <li>
-              <img src={react} alt="edho" />
-            </li>
-            <li>
-              <img
-                src="https://asset.brandfetch.io/id2alue-rx/id9K4_Y8_G.jpeg"
-                alt="edho"
-              />
-            </li>
-            <li>
-              <img
-                src="https://w7.pngwing.com/pngs/956/695/png-transparent-mongodb-original-wordmark-logo-icon-thumbnail.png"
-                alt="edho"
-              />
-            </li>
-            <li>
-              <img
-                src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/53819/1.png"
-                alt="edho"
-              />
-            </li>
-            <li>
-              <img src={node} alt="edho" />
-            </li>
-            <li>
-              <img
-                src="https://upload.wikimedia.org/wikipedia/commons/6/6a/JavaScript-logo.png"
-                alt="edho"
-              />
-            </li>
-            <li>
-              <img
-                src="https://m.media-amazon.com/images/I/41N4YPqlxfL._AC_UF1000,1000_QL80_.jpg"
-                alt="edho"
-              />
-            </li>
-            <li>
-              <img
-                src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/53819/8.png"
-                alt="edho"
-              />
-            </li>
-          </ul>
+      <div className="boxxxx">
+        <div class="grid">
+          <div>
+            <img src={react} alt="react" />
+          </div>
+          <div>
+            <img src={node} alt="react" />
+          </div>
+          <div>
+            <img src={next} alt="react" />
+          </div>
+          <div>
+            <img src={python} alt="react" />
+          </div>
+          <div>
+            <img src={mongodb} alt="react" />
+          </div>
+
+          <div>
+            <img src={js} alt="react" />
+          </div>
         </div>
       </div>
     </div>
